62a28572008fab7462a724d9fccd3654b490462c
Daniel Díaz <daniel.diaz@linaro.org>
2021-03-23 11:19:02 -0600

    new dir
    Signed-off-by: Daniel Díaz <daniel.diaz@linaro.org>

--- automated/linux/packetdrill/packetdrill.sh
+++ automated/linux/packetdrill/packetdrill.sh
@@ -123,7 +123,7 @@ build_install_tests() {
 run_test() {
 #    pushd "${TEST_DIR}/gtests/net" || exit 1
 #    python3 ./packetdrill/run_all.py -v -l -L  2>&1 | tee -a "${RESULT_LOG}"
-    pushd "${TEST_DIR}" || exit 1
+    pushd "${TEST_DIR}"/packetdrill || exit 1
     pwd
 #    cd ..
     python3 run_all.py -v -l -L  2>&1 | tee -a "${RESULT_LOG}"
