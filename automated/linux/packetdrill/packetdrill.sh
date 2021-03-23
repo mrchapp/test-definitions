b68b9ed2ff4e6fb7074a7fad826e38a6fc387d36
Daniel Díaz <daniel.diaz@linaro.org>
2021-03-23 16:40:14 -0600

    packetdrill: Fix execution directory
    Packetdrill expects things to be in certain places, and it
    seems to be important to be in the given directory when
    calling the main runner (run_all.py) so that all references
    can be located.
    
    Signed-off-by: Daniel Díaz <daniel.diaz@linaro.org>

--- automated/linux/packetdrill/packetdrill.sh
+++ automated/linux/packetdrill/packetdrill.sh
@@ -121,7 +121,7 @@ build_install_tests() {
 }
 
 run_test() {
-    pushd "${TEST_DIR}/gtests/net" || exit 1
+    pushd "${TEST_DIR}" || exit 1
     python3 ./packetdrill/run_all.py -v -l -L  2>&1 | tee -a "${RESULT_LOG}"
     popd || exit 1
 }
